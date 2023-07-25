58a5962571b12a833d1aff28e6e207661a22000d
ISABELE DOS SANTOS RIBEIRO <isabele.sribeiro@senacsp.edu.br>
2023-07-25 20:17:53 -0300

    Configurei todas as txt no button Salvar, tambem configurei o Botão limpar

--- Aula1/Aula 1 - respondendo perguntas/FrmIsabele.Designer.cs
+++ Aula1/Aula 1 - respondendo perguntas/FrmIsabele.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Aula_1___respondendo_perguntas
 {
-    partial class FrmIsabele
+    partial class txtLimpar
     {
         /// <summary>
         /// Required designer variable.
@@ -29,33 +29,228 @@
         private void InitializeComponent()
         {
             this.label = new System.Windows.Forms.Label();
-            this.txtNomeProduto = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtLote = new System.Windows.Forms.TextBox();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.DTPValidade = new System.Windows.Forms.DateTimePicker();
+            this.DTPFabricacao = new System.Windows.Forms.DateTimePicker();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtCategoria = new System.Windows.Forms.TextBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label
             // 
             this.label.AutoSize = true;
-            this.label.Location = new System.Drawing.Point(36, 48);
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.Location = new System.Drawing.Point(223, 38);
             this.label.Name = "label";
-            this.label.Size = new System.Drawing.Size(93, 13);
+            this.label.Size = new System.Drawing.Size(170, 24);
             this.label.TabIndex = 0;
-            this.label.Text = "Nome do Produto ";
+            this.label.Text = "Nome do Produto :";
+            this.label.Click += new System.EventHandler(this.label_Click);
             // 
-            // txtNomeProduto
+            // txtNome
             // 
-            this.txtNomeProduto.Location = new System.Drawing.Point(179, 45);
-            this.txtNomeProduto.Name = "txtNomeProduto";
-            this.txtNomeProduto.Size = new System.Drawing.Size(100, 20);
-            this.txtNomeProduto.TabIndex = 1;
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(571, 38);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(382, 29);
+            this.txtNome.TabIndex = 1;
+            this.txtNome.TextChanged += new System.EventHandler(this.txtNome_TextChanged);
             // 
-            // FrmIsabele
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(224, 274);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Data Validade:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(224, 226);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(178, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Data de Fabricação:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(224, 184);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Lote:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(224, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(197, 24);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Descrição do Produto:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(223, 384);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(179, 24);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Codigo do Produto :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(224, 328);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(152, 24);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Valor do Produto";
+            // 
+            // txtValor
+            // 
+            this.txtValor.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValor.Location = new System.Drawing.Point(571, 328);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(382, 29);
+            this.txtValor.TabIndex = 9;
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCodigo.Location = new System.Drawing.Point(571, 384);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(382, 29);
+            this.txtCodigo.TabIndex = 10;
+            // 
+            // txtLote
+            // 
+            this.txtLote.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLote.Location = new System.Drawing.Point(571, 184);
+            this.txtLote.Name = "txtLote";
+            this.txtLote.Size = new System.Drawing.Size(382, 29);
+            this.txtLote.TabIndex = 11;
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDescricao.Location = new System.Drawing.Point(571, 138);
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(382, 29);
+            this.txtDescricao.TabIndex = 12;
+            // 
+            // DTPValidade
+            // 
+            this.DTPValidade.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DTPValidade.Location = new System.Drawing.Point(571, 269);
+            this.DTPValidade.Name = "DTPValidade";
+            this.DTPValidade.Size = new System.Drawing.Size(382, 29);
+            this.DTPValidade.TabIndex = 13;
+            // 
+            // DTPFabricacao
+            // 
+            this.DTPFabricacao.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DTPFabricacao.Location = new System.Drawing.Point(571, 226);
+            this.DTPFabricacao.Name = "DTPFabricacao";
+            this.DTPFabricacao.Size = new System.Drawing.Size(382, 29);
+            this.DTPFabricacao.TabIndex = 14;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnSalvar.Location = new System.Drawing.Point(703, 509);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(183, 40);
+            this.btnSalvar.TabIndex = 15;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = false;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.panel1.Location = new System.Drawing.Point(496, 7);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(10, 577);
+            this.panel1.TabIndex = 16;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(223, 89);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(198, 24);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Categoria do Produto :";
+            // 
+            // txtCategoria
+            // 
+            this.txtCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCategoria.Location = new System.Drawing.Point(571, 84);
+            this.txtCategoria.Name = "txtCategoria";
+            this.txtCategoria.Size = new System.Drawing.Size(382, 29);
+            this.txtCategoria.TabIndex = 18;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnLimpar.Location = new System.Drawing.Point(252, 513);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(169, 36);
+            this.btnLimpar.TabIndex = 19;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // txtLimpar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.txtNomeProduto);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(1078, 595);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.txtCategoria);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.DTPFabricacao);
+            this.Controls.Add(this.DTPValidade);
+            this.Controls.Add(this.txtDescricao);
+            this.Controls.Add(this.txtLote);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNome);
             this.Controls.Add(this.label);
-            this.Name = "FrmIsabele";
+            this.Name = "txtLimpar";
             this.Text = "Cadastro do Produto";
             this.Load += new System.EventHandler(this.FrmIsabele_Load);
             this.ResumeLayout(false);
@@ -66,6 +261,23 @@
         #endregion
 
         private System.Windows.Forms.Label label;
-        private System.Windows.Forms.TextBox txtNomeProduto;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtLote;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.DateTimePicker DTPValidade;
+        private System.Windows.Forms.DateTimePicker DTPFabricacao;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtCategoria;
+        private System.Windows.Forms.Button btnLimpar;
     }
 }
--- Aula1/Aula 1 - respondendo perguntas/FrmIsabele.cs
+++ Aula1/Aula 1 - respondendo perguntas/FrmIsabele.cs
@@ -4,15 +4,16 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Aula_1___respondendo_perguntas
 {
-    public partial class FrmIsabele : Form
+    public partial class txtLimpar : Form
     {
-        public FrmIsabele()
+        public txtLimpar()
         {
             InitializeComponent();
         }
@@ -20,6 +21,62 @@ namespace Aula_1___respondendo_perguntas
         private void FrmIsabele_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void label_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            String Validade = DTPValidade.Value.ToString("yyyy-MM-dd");
+            String Fabricacao = DTPFabricacao.Value.ToString("yyyy-MM-dd");
+            string codigo = txtCodigo.Text;
+            string Lote = txtLote.Text;
+            string Nome = txtNome.Text;
+            string Valor = txtValor.Text;
+            string Categoria = txtCategoria.Text;
+            string mensagem = $"Nome do Produto: {Nome}\n" +
+
+                              $"Código de Barras: {codigo}\n" +
+
+                              $"Lote: {Lote}\n" +
+
+                              $"Categoria do Produto:{Categoria}\n"+
+                              
+                              $"Valor: {Valor:C}\n" +
+
+                              $"Vencimento: {Validade}\n" +
+                            
+                              $"Data de Fabricação: {Fabricacao}";
+
+            //Ao inves do menssage box jogar para o banco de dados 
+
+            MessageBox.Show(mensagem, "Informações do Produto", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+
+
+
+
+        }
+
+        private void txtNome_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtCodigo.Text = string.Empty;
+            txtLote.Text = string.Empty;
+            txtNome.Text = string.Empty;
+            txtCategoria.Text = string.Empty;
+            txtDescricao.Text = string.Empty;   
+            txtValor.Text = string.Empty;
+            DTPValidade.Value = DateTime.Now;
+            DTPFabricacao.Value = DateTime.Now;
         }
     }
 }
