7c32f8aaacab2fc5115e0754b3b1ee4cfed6cf6b
ISABELE DOS SANTOS RIBEIRO <isabele.sribeiro@senacsp.edu.br>
2023-07-24 22:04:58 -0300

    fiz uma txt com o nome do produto

---
+++ Aula1/Aula 1 - respondendo perguntas/FrmIsabele.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Aula_1___respondendo_perguntas
+{
+    partial class FrmIsabele
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label = new System.Windows.Forms.Label();
+            this.txtNomeProduto = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(36, 48);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(93, 13);
+            this.label.TabIndex = 0;
+            this.label.Text = "Nome do Produto ";
+            // 
+            // txtNomeProduto
+            // 
+            this.txtNomeProduto.Location = new System.Drawing.Point(179, 45);
+            this.txtNomeProduto.Name = "txtNomeProduto";
+            this.txtNomeProduto.Size = new System.Drawing.Size(100, 20);
+            this.txtNomeProduto.TabIndex = 1;
+            // 
+            // FrmIsabele
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtNomeProduto);
+            this.Controls.Add(this.label);
+            this.Name = "FrmIsabele";
+            this.Text = "Cadastro do Produto";
+            this.Load += new System.EventHandler(this.FrmIsabele_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.TextBox txtNomeProduto;
+    }
+}
---
+++ Aula1/Aula 1 - respondendo perguntas/FrmIsabele.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula_1___respondendo_perguntas
+{
+    public partial class FrmIsabele : Form
+    {
+        public FrmIsabele()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmIsabele_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
