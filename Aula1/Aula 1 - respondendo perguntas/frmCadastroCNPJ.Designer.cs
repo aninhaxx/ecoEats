30bb675a7fd6f229f015efe49a6ebd7cff2b3089
lana_verdete <verdetealana@gmail.com>
2023-07-24 22:16:46 -0300

    Criei um formulário para cadastro do cliente que possui CNPJ

---
+++ Aula1/Aula 1 - respondendo perguntas/frmCadastroCNPJ.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Aula_1___respondendo_perguntas
+{
+    partial class frmCadastroCNPJ
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCNPJ = new System.Windows.Forms.Label();
+            this.lblRazaoSocial = new System.Windows.Forms.Label();
+            this.lblNomeFantasia = new System.Windows.Forms.Label();
+            this.lblEndereco = new System.Windows.Forms.Label();
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.lblBairro = new System.Windows.Forms.Label();
+            this.lblCep = new System.Windows.Forms.Label();
+            this.lblContato = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblCNPJ
+            // 
+            this.lblCNPJ.AutoSize = true;
+            this.lblCNPJ.Location = new System.Drawing.Point(21, 34);
+            this.lblCNPJ.Name = "lblCNPJ";
+            this.lblCNPJ.Size = new System.Drawing.Size(37, 13);
+            this.lblCNPJ.TabIndex = 0;
+            this.lblCNPJ.Text = "CNPJ:";
+            // 
+            // lblRazaoSocial
+            // 
+            this.lblRazaoSocial.AutoSize = true;
+            this.lblRazaoSocial.Location = new System.Drawing.Point(21, 75);
+            this.lblRazaoSocial.Name = "lblRazaoSocial";
+            this.lblRazaoSocial.Size = new System.Drawing.Size(73, 13);
+            this.lblRazaoSocial.TabIndex = 1;
+            this.lblRazaoSocial.Text = "Razão Social:";
+            // 
+            // lblNomeFantasia
+            // 
+            this.lblNomeFantasia.AutoSize = true;
+            this.lblNomeFantasia.Location = new System.Drawing.Point(21, 119);
+            this.lblNomeFantasia.Name = "lblNomeFantasia";
+            this.lblNomeFantasia.Size = new System.Drawing.Size(81, 13);
+            this.lblNomeFantasia.TabIndex = 2;
+            this.lblNomeFantasia.Text = "Nome Fantasia:";
+            // 
+            // lblEndereco
+            // 
+            this.lblEndereco.AutoSize = true;
+            this.lblEndereco.Location = new System.Drawing.Point(21, 158);
+            this.lblEndereco.Name = "lblEndereco";
+            this.lblEndereco.Size = new System.Drawing.Size(56, 13);
+            this.lblEndereco.TabIndex = 3;
+            this.lblEndereco.Text = "Endereço:";
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Location = new System.Drawing.Point(21, 197);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(19, 13);
+            this.lblNumero.TabIndex = 4;
+            this.lblNumero.Text = "N°";
+            // 
+            // lblBairro
+            // 
+            this.lblBairro.AutoSize = true;
+            this.lblBairro.Location = new System.Drawing.Point(21, 235);
+            this.lblBairro.Name = "lblBairro";
+            this.lblBairro.Size = new System.Drawing.Size(37, 13);
+            this.lblBairro.TabIndex = 5;
+            this.lblBairro.Text = "Bairro:";
+            // 
+            // lblCep
+            // 
+            this.lblCep.AutoSize = true;
+            this.lblCep.Location = new System.Drawing.Point(21, 272);
+            this.lblCep.Name = "lblCep";
+            this.lblCep.Size = new System.Drawing.Size(31, 13);
+            this.lblCep.TabIndex = 6;
+            this.lblCep.Text = "CEP:";
+            // 
+            // lblContato
+            // 
+            this.lblContato.AutoSize = true;
+            this.lblContato.Location = new System.Drawing.Point(21, 305);
+            this.lblContato.Name = "lblContato";
+            this.lblContato.Size = new System.Drawing.Size(52, 13);
+            this.lblContato.TabIndex = 7;
+            this.lblContato.Text = "Telefone:";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(21, 338);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(38, 13);
+            this.lblEmail.TabIndex = 8;
+            this.lblEmail.Text = "E-mail:";
+            // 
+            // frmCadastroCNPJ
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 650);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblContato);
+            this.Controls.Add(this.lblCep);
+            this.Controls.Add(this.lblBairro);
+            this.Controls.Add(this.lblNumero);
+            this.Controls.Add(this.lblEndereco);
+            this.Controls.Add(this.lblNomeFantasia);
+            this.Controls.Add(this.lblRazaoSocial);
+            this.Controls.Add(this.lblCNPJ);
+            this.Name = "frmCadastroCNPJ";
+            this.Text = "frmCadastroCNPJ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCNPJ;
+        private System.Windows.Forms.Label lblRazaoSocial;
+        private System.Windows.Forms.Label lblNomeFantasia;
+        private System.Windows.Forms.Label lblEndereco;
+        private System.Windows.Forms.Label lblNumero;
+        private System.Windows.Forms.Label lblBairro;
+        private System.Windows.Forms.Label lblCep;
+        private System.Windows.Forms.Label lblContato;
+        private System.Windows.Forms.Label lblEmail;
+    }
+}
