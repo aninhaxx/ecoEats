482a195857dbf9ea520b9549404aa615d9a6f09e
ANINHAXX <ana.CORONA.CAROLINA222@GMAIL.COM>
2023-07-24 22:06:02 -0300

    coloquei uma label

---
+++ Aula1/Aula 1 - respondendo perguntas/frmAlterarInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula_1___respondendo_perguntas
+{
+    public partial class frmAlterarInfo : Form
+    {
+        public frmAlterarInfo()
+        {
+            InitializeComponent();
+        }
+    }
+}
