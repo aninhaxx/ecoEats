1abca63e0319614248cedb0bce369b13a238f334
VIVIAN DA SILVA FREITAS <vivian.sfreitas1@senacsp.edu.br>
2023-07-26 21:47:03 -0300

    Terminei e desing e comecei a programação

---
+++ Aula1/Aula 1 - respondendo perguntas/FrmCódigodeBarras.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Aula_1___respondendo_perguntas
+{
+    partial class FrmCódigodeBarras
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDigiteCod = new System.Windows.Forms.Button();
+            this.lblCódBarras = new System.Windows.Forms.Label();
+            this.btnContinuar = new System.Windows.Forms.Button();
+            this.btnVoltar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDigiteCod
+            // 
+            this.btnDigiteCod.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDigiteCod.ForeColor = System.Drawing.Color.Gray;
+            this.btnDigiteCod.Location = new System.Drawing.Point(120, 170);
+            this.btnDigiteCod.Name = "btnDigiteCod";
+            this.btnDigiteCod.Size = new System.Drawing.Size(465, 57);
+            this.btnDigiteCod.TabIndex = 0;
+            this.btnDigiteCod.Text = "Digite ou cole o código";
+            this.btnDigiteCod.UseVisualStyleBackColor = true;
+            // 
+            // lblCódBarras
+            // 
+            this.lblCódBarras.AutoSize = true;
+            this.lblCódBarras.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCódBarras.Location = new System.Drawing.Point(125, 146);
+            this.lblCódBarras.Name = "lblCódBarras";
+            this.lblCódBarras.Size = new System.Drawing.Size(139, 21);
+            this.lblCódBarras.TabIndex = 2;
+            this.lblCódBarras.Text = "Código de barras";
+            // 
+            // btnContinuar
+            // 
+            this.btnContinuar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnContinuar.FlatAppearance.BorderSize = 0;
+            this.btnContinuar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnContinuar.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnContinuar.ForeColor = System.Drawing.Color.White;
+            this.btnContinuar.Location = new System.Drawing.Point(307, 233);
+            this.btnContinuar.Name = "btnContinuar";
+            this.btnContinuar.Size = new System.Drawing.Size(110, 37);
+            this.btnContinuar.TabIndex = 3;
+            this.btnContinuar.Text = "Continuar";
+            this.btnContinuar.UseVisualStyleBackColor = false;
+            this.btnContinuar.Click += new System.EventHandler(this.btnContinuar_Click);
+            // 
+            // btnVoltar
+            // 
+            this.btnVoltar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVoltar.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVoltar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnVoltar.Location = new System.Drawing.Point(307, 276);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.Size = new System.Drawing.Size(110, 32);
+            this.btnVoltar.TabIndex = 4;
+            this.btnVoltar.Text = "Voltar";
+            this.btnVoltar.UseVisualStyleBackColor = true;
+            this.btnVoltar.Click += new System.EventHandler(this.btnVoltar_Click);
+            // 
+            // FrmCódigodeBarras
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnVoltar);
+            this.Controls.Add(this.btnContinuar);
+            this.Controls.Add(this.lblCódBarras);
+            this.Controls.Add(this.btnDigiteCod);
+            this.Name = "FrmCódigodeBarras";
+            this.Text = "FrmCódigodeBarras";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDigiteCod;
+        private System.Windows.Forms.Label lblCódBarras;
+        private System.Windows.Forms.Button btnContinuar;
+        private System.Windows.Forms.Button btnVoltar;
+    }
+}
---
+++ Aula1/Aula 1 - respondendo perguntas/FrmCódigodeBarras.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula_1___respondendo_perguntas
+{
+    public partial class FrmCódigodeBarras : Form
+    {
+        public FrmCódigodeBarras()
+        {
+            InitializeComponent();
+        }
+
+        private void btnContinuar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("");
+        }
+
+        private void btnVoltar_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
