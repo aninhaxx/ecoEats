cf04df98db57dd49d9baab3d91583653576bf37d
BEATRIZ LEAL <beatriz.leal@senacsp.edu.br>
2023-07-28 19:18:06 -0300

    Alterei a cor da fonte da Label "Cadastre com o CPF"

--- Aula1/Aula 1 - respondendo perguntas/FrmCadastroCPF.Designer.cs
+++ Aula1/Aula 1 - respondendo perguntas/FrmCadastroCPF.Designer.cs
@@ -69,7 +69,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.ForeColor = System.Drawing.Color.Snow;
             this.label1.Location = new System.Drawing.Point(12, 22);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(159, 21);
